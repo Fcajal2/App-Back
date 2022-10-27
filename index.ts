b36ca7a1c6dfbd22917a53d54aa95de93f3265f4
Fcajal2 <daikuzu@hotmail.com>
2022-10-27 19:23:31 -0300

    edit

--- index.ts
+++ index.ts
@@ -6,15 +6,17 @@ app.use(cors());
 
 app.use(express.json());
 
-const products: { id: number; name: string; marca: string }[] = [
+const products: { id: number; name: string; brand: string; price: number }[] = [
   {
     name: "Teclado",
-    marca: "Logitech",
+    brand: "Logitech",
+    price: 12,
     id: 2131231,
   },
   {
     name: "Placa",
-    marca: "AMD",
+    brand: "AMD",
+    price: 15,
     id: 21312312,
   },
 ];
@@ -42,12 +44,12 @@ app.get("/product/:id", (req, res) => {
  * Create product
  */
 app.post("/product", (req, res) => {
-  const { name, marca } = req.body;
-  if (!name || !marca)
+  const { name, brand, price } = req.body;
+  if (!name || !brand)
     return res
       .status(400)
-      .json({ message: "fields 'name' and 'marca' is requiered" });
-  products.push({ id: new Date().getTime(), name, marca });
+      .json({ message: "fields 'name' and 'marca' is required" });
+  products.push({ id: new Date().getTime(), name, brand, price });
   return res.status(200).json(...products.slice(-1));
 });
 
@@ -55,14 +57,14 @@ app.post("/product", (req, res) => {
  * Edit product by id
  */
 app.put("/product/:id", (req, res) => {
-  const { name, marca } = req.body;
+  const { name, brand } = req.body;
   const id = Number(req.params.id);
   const index = products.findIndex((product) => product.id === id);
 
   if (index === -1)
     return res.status(400).json({ message: "product could not be found" });
-  if (marca) {
-    products[index].marca = marca;
+  if (brand) {
+    products[index].brand = brand;
   }
   if (name) {
     products[index].name = name;
@@ -83,4 +85,4 @@ app.delete("/product/:id", (req, res) => {
   return res.status(200).json(products.splice(index, 1));
 });
 
-app.listen(3000);
+app.listen(3001);
